ce3d8bdf2af9d454dd646722b940bfc4f060753e
Nadir23 <boutra.nadir@gmail.com>
2018-07-23 18:36:50 +0200

    class binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   public name = "Nadir" ;
-  public myId = "testId" ;
-  public isDisabled = false ;
+  public successClass = "text-success";
+  public hasError = true ;
+  public isSpecial = true ;
+  public messageClasses = {
+    "text-success": !this.hasError ,
+    "text-danger" : this.hasError ,
+    "text-special": this.isSpecial,
+  }
 
   constructor() { }
 
