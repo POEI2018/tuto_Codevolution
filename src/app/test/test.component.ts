8280a136ee99d20673e5f15e221aa538187ffb29
Nadir23 <boutra.nadir@gmail.com>
2018-07-25 22:46:51 +0200

    Components interaction

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -9,10 +9,17 @@ export class TestComponent implements OnInit {
 
   public colors= ["red", "blue", "yellow", "black"] ;
   
+  @Input() public parentData ;
+
+  @Output() public childEvent = new EventEmitter() ;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public fireEvent() {
+    this.childEvent.emit('Code evolution');
   }
 
  
